64c2dd0ddece36b52267c538d369fb8974fda23f
simon <657258071@qq.com>
2015-12-10 14:37:05 +0800

    sss

--- public/app/nanyuan/pagejs/documents5.js
+++ public/app/nanyuan/pagejs/documents5.js
@@ -108,10 +108,8 @@ var documentsModel=createModel('documents',function(modelName){
         vm.init=function(){
             if(clientInfoModel.$initializeStatus=='Y'){
                 vm.initApp();
-                console.log('4444');
             }else{
                 clientInfoModel.addInitializeHandler(function(cb){
-                    console.log('5555');
                     documentsModel.initApp();
                     if(cb){cb();}
                 });
